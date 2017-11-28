9578e336f76b1f53421bbbeb0c844f3e4f9fad7f
Andy Gomez <andygomezb7@gmail.com>
2017-11-27 23:50:51 -0600

    Menu and parcial header

--- src/App.js
+++ src/App.js
@@ -11,6 +11,22 @@ class App extends Component {
           <div className="bgColor"></div>
           <div className="bgBlack"></div>
           <section className="spotifyContent container">
+            <div className="row flex-row">
+              <div className="col-xs-12 col-md-4 hero-intro">
+                <small>Eschado recientemente</small>
+                <h1 class="display-2 premium-display-2">Retoma el ritmo</h1>
+                <h3>Sigue disfrutando de la música que estabas escuchando justo donde la dejaste.</h3>
+              </div>
+              <div className="col-xs-12 col-md-8">
+                <div class="row">
+                  <div class="spotifyAlbumList">
+                    <ul>
+                      <li></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
             <Footer />
           </section>
         </section>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ class Header extends React.Component {
 
 	render() {
 		return (
-			<header className="navbar navbar-default navbar-fixed-top navbar-transparent navbar-highlight-black">
+			<header className="navbar navbar-default navbar-fixed-top navbar-transparent navbar-highlight-black" onScroll={ this.onScroll }>
 				<div className="container">
 					<div className="navbar-header">
 						<button type="button" className="navbar-toggle collapsed" data-toggle="sidepanel" data-target="#navbar-nav">
@@ -20,27 +20,30 @@ class Header extends React.Component {
 					</div>
 					<div className="collapse navbar-collapse" role="navigation" id="navbar-nav">
 						<ul className="nav navbar-nav navbar-right nav-main">
-							<li>
+							<li className="principal">
 								<a href="">Premium</a>
 							</li>
-							<li>
+							<li className="principal">
 								<a href="">Ayuda</a>
 							</li>
-							<li>
+							<li className="principal">
 								<a href="">Descargar</a>
 							</li>
 							<li role="separator" className="divider"></li>
-							<li>
+							<li  className="principal">
 								<Dropdown id="dropdownUser">
 									<Dropdown.Toggle>
 										<img src="https://ruclip.com/chimg/84/UC3biv9gIP2KiaBmBy8Phe0Q.jpg" style={{width:'40px',height:'40px'}} className="img-circle" alt="Logo" /> 
-										<span>Perfil</span>
-										<span></span>
+										<span className="profileText">Perfil</span>
+										<span className="caret-bottom">
+											<svg id="chevron-down" viewBox="0 0 1024 1024" width="100%" height="100%">
+											  <path className="path1" d="M476.455 806.696l-381.164-381.164q-14.621-14.621-14.621-35.293t14.621-34.789 35.293-14.117 34.789 14.117l342.846 343.35 349.4-349.4q14.621-14.117 35.293-14.117t34.789 14.117 14.117 34.789-14.117 34.789l-381.164 381.164q-19.159 19.159-38.318 19.159t-31.764-12.605z"></path>
+											</svg>
+										</span>
 									</Dropdown.Toggle>
 									<Dropdown.Menu>
-										<MenuItem>Premium</MenuItem>
-										<MenuItem>Ayuda</MenuItem>
-										<MenuItem>Descargar</MenuItem>
+										<MenuItem>Cuenta</MenuItem>
+										<MenuItem className="logout-link">Cerrar Sesión</MenuItem>
 									</Dropdown.Menu>
 								</Dropdown>
 							</li>
@@ -50,6 +53,10 @@ class Header extends React.Component {
 			</header>
 		)
 	}
+
+	onScroll(r, s) {
+        console.log('s', s);
+    }
 
 }
 
